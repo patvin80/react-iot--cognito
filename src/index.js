15af3c329717deb57f0311507443a369c03061fd
Vinit Patankar <vinkar@amazon.com>
2018-05-22 11:07:40 -0400

    Doing a conditional dotenv setup and checking if the build works

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import dotenv from 'dotenv'
-dotenv.config()
-
+if (process.env.NODE_ENV !== 'production')
+{  
+    dotenv.config()
+}
 ReactDOM.render(
     <Router>
         <App />
